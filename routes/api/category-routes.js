65db0ddccd3404ceb8c385740f9b5d21f77f5ab3
Kurohyou <scaseydv@gmail.com>
2022-09-10 16:34:46 -0600

    Category routes completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,16 +35,36 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const cat = await Category.create({category_name:req.body.category_name});
+    res.json(cat);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const cat = await (await Category.findByPk(req.params.id))
+      .update({category_name:req.body.category_name});
+    res.json(cat);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const cat = await Category.findByPk(req.params.id);
+    await cat.destroy();
+    res.json(cat);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
